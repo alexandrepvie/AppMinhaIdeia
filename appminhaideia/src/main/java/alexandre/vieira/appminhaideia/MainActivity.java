ee82e5b2a440f5cdc4730232c5249a854179e30a
giant <alexandrepvie@gmail.com>
2021-03-19 20:38:38 -0300

    Aula 14 Concluida. duas telas e a troca de telas

--- appminhaideia/src/main/java/alexandre/vieira/appminhaideia/MainActivity.java
+++ appminhaideia/src/main/java/alexandre/vieira/appminhaideia/MainActivity.java
@@ -3,12 +3,17 @@ package alexandre.vieira.appminhaideia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    String TAG = "APP_MINHA_IDEIA";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+                /**Fazendo teste no GitHub6*/
+        Log.d(TAG, "onCreate: Tela principal carregada...");
     }
 }
